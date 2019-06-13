173a86475a176157f4e95b32280f59d9dc99433d
Pandav <anand.pandav@sky.uk>
2019-06-13 09:52:44 +0100

    Updated app

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Updated version 1.
+          Updated version AWS 2.
         </p>
         <a
           className="App-link"
